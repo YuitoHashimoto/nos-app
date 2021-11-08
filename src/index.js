19e65b14638662feda29297e8f7f380808542009
hashimoto.yit@intern20w <hashimoto.yit@gmail.com>
2021-11-08 14:04:46 +0900

    firebase secction

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 // firebase
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
