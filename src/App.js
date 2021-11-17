290201c487a1ab806e5f5e0d461a0f3229b04005
hashimoto.yit@intern20w <hashimoto.yit@gmail.com>
2021-11-17 11:28:44 +0900

    load

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './scss/index.scss';
 import{ LoadView } from './components/index';
 import {Routes, Route } from 'react-router-dom';
 // import firebase from './firebase';
--- src/components/LoadView.jsx
+++ src/components/LoadView.jsx
@@ -1,6 +1,20 @@
 const LoadView = () => {
     return (
-        <h1>ロード</h1>
+        <main className={'loading'}>
+            <div className={'loading__imgs'}>
+                <div className={'loading__imgs__content'}></div>
+                <div className={'loading__imgs__content'}></div>
+                <div className={'loading__imgs__content'}></div>
+            </div>
+            <div className={'loading__txt'}>
+                <h1>Loading</h1>
+            </div>
+            <div className={'loading__imgs imgsBottom'}>
+                <div className={'loading__imgs__content'}></div>
+                <div className={'loading__imgs__content'}></div>
+                <div className={'loading__imgs__content'}></div>
+            </div>
+        </main>
     )
 }
 
